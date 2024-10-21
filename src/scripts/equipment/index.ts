84e538e3046ffdf52cd5b7f703b88c23003211fb
Igorrafael90 <rafaeligor804@gmail.com>
2024-10-20 21:32:06 -0300

    add potions

--- src/scripts/equipment/index.ts
+++ src/scripts/equipment/index.ts
@@ -4,4 +4,15 @@ const weapons: weaponEquipType[] = [];
 
 const armor: defenseEquipType[] = [];
 
-const potion: potions[] = [];
+const potion: potions[] = [
+    {
+        name: "Health potion",
+        health: 5,
+        mana: 0,
+    },
+    {
+        name: "Mana potion",
+        health: 0,
+        mana: 5,
+    }
+];
