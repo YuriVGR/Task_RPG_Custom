6add6cbbdcd46780b34bf140baa36d21ce1ce03c
YuriVGR <yurivgr@icloud.com>
2024-10-24 20:54:54 -0300

    xD

--- src/routes/tabs.routes.tsx
+++ src/routes/tabs.routes.tsx
@@ -3,12 +3,25 @@ import HomeScreen from "../screens/home";
 import SettingScreen from "../screens/settings";
 import TaskScreen from "../screens/task";
 import RPGScreen from "../screens/rolepg";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabRoutes() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName = "house"
+          if (route.name === "Home") {
+            iconName === "hoime"
+          } if (route.name === "RPG") {
+            iconName === "sword"
+          }
+          return <></>
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="RPG" component={RPGScreen} />
       <Tab.Screen name="Tasks" component={TaskScreen} />
