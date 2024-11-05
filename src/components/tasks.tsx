7984576a01be330830e4bf3d933e4412c892010b
YuriVGR <yurivgr@icloud.com>
2024-11-05 15:05:15 -0300

    ?

--- src/components/tasks.tsx
+++ src/components/tasks.tsx
@@ -1,143 +1,148 @@
 import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-import { Task } from "../interfaces";
+import { Task } from "../types/Task";
 
 interface TaskComponentProps extends Task {
-  removeTask: (taskId: string) => void;
+    removeTask: (taskId: string) => void;
 }
 
 export default function TaskComponent({
-  id,
-  title,
-  description,
-  status,
-  removeTask,
+    id,
+    title,
+    description,
+    status,
+    removeTask,
 }: TaskComponentProps) {
-  const currentStatus = (status: number) => {
-    if (status === 1) {
-      return "Pendente";
-    }
-    if (status === 2) {
-      return "Concluído";
-    }
-    if (status === 3) {
-      return "Cancelado";
-    }
-    return "Desconhecido";
-  };
+    const currentStatus = (status: number) => {
+        if (status === 1) {
+            return "Pendente";
+        }
+        if (status === 2) {
+            return "Concluído";
+        }
+        if (status === 3) {
+            return "Cancelado";
+        }
+        return "Desconhecido";
+    };
 
-  const currentColor = (status: number) => {
-    if (status === 1) {
-      return "#E6B800"; // Darker yellow
-    }
-    if (status === 2) {
-      return "#4CAF50"; // Green
-    }
-    if (status === 3) {
-      return "#FF4D4D"; // Red
-    }
-    return "#A9A9A9"; // Gray for unknown
-  };
+    const currentColor = (status: number) => {
+        if (status === 1) {
+            return "#E6B800"; // Darker yellow
+        }
+        if (status === 2) {
+            return "#4CAF50"; // Green
+        }
+        if (status === 3) {
+            return "#FF4D4D"; // Red
+        }
+        return "#A9A9A9"; // Gray for unknown
+    };
 
-  const currentBGColor = (status: number) => {
-    if (status === 1) {
-      return "#FFF9B0"; // Lighter yellow background
-    }
-    if (status === 2) {
-      return "#A5D6A7"; // Lighter green background
-    }
-    if (status === 3) {
-      return "#FFB3B3"; // Lighter red background
-    }
-    return "#D3D3D3"; // Gray for unknown
-  };
+    const currentBGColor = (status: number) => {
+        if (status === 1) {
+            return "#FFF9B0"; // Lighter yellow background
+        }
+        if (status === 2) {
+            return "#A5D6A7"; // Lighter green background
+        }
+        if (status === 3) {
+            return "#FFB3B3"; // Lighter red background
+        }
+        return "#D3D3D3"; // Gray for unknown
+    };
 
-  return (
-    <View
-      style={[
-        styles.taskContainer,
-        {
-          backgroundColor: currentBGColor(status),
-          borderColor: currentColor(status),
-        },
-      ]}
-    > 
-      <View style={styles.header}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-      <View style={styles.middle}>
-        <Text style={styles.description}>{description}</Text>
-      </View>
-      <View style={styles.footer}>
-        <View style={styles.footerL}>
-          <FontAwesome6
-            name="circle-exclamation"
-            size={20}
-            color={currentColor(status)}
-          />
-          <Text style={{ color: currentColor(status), fontWeight: "bold" }}>
-            {currentStatus(status)}
-          </Text>
+    return (
+        <View
+            style={[
+                styles.taskContainer,
+                {
+                    backgroundColor: currentBGColor(status),
+                    borderColor: currentColor(status),
+                },
+            ]}
+        >
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+            <View style={styles.middle}>
+                <Text style={styles.description}>{description}</Text>
+            </View>
+            <View style={styles.footer}>
+                <View style={styles.footerL}>
+                    <FontAwesome6
+                        name="circle-exclamation"
+                        size={20}
+                        color={currentColor(status)}
+                    />
+                    <Text
+                        style={{
+                            color: currentColor(status),
+                            fontWeight: "bold",
+                        }}
+                    >
+                        {currentStatus(status)}
+                    </Text>
+                </View>
+                <View style={styles.footerR}>
+                    <TouchableOpacity onPress={() => removeTask(id)}>
+                        <FontAwesome6
+                            name="trash-can"
+                            size={20}
+                            color={currentColor(status)}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <FontAwesome6
+                            name="pen-to-square"
+                            size={20}
+                            color={currentColor(status)}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
-        <View style={styles.footerR}>
-          <TouchableOpacity onPress={() => removeTask(id)}>
-            <FontAwesome6
-              name="trash-can"
-              size={20}
-              color={currentColor(status)}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome6
-              name="pen-to-square"
-              size={20}
-              color={currentColor(status)}
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  taskContainer: {
-    width: "100%",
-    paddingHorizontal: 5,
-    borderRadius: 16,
-    borderWidth: 1,
-  },
-  header: {
-    padding: 10,
-    borderBottomWidth: 1,
-  },
-  middle: {
-    padding: 10,
-    paddingBottom: 20,
-  },
-  footer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    borderTopWidth: 1,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  description: {
-    fontSize: 16,
-  },
-  footerL: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-  },
-  footerR: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-  },
+    taskContainer: {
+        width: "100%",
+        paddingHorizontal: 5,
+        borderRadius: 16,
+        borderWidth: 1,
+    },
+    header: {
+        padding: 10,
+        borderBottomWidth: 1,
+    },
+    middle: {
+        padding: 10,
+        paddingBottom: 20,
+    },
+    footer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        borderTopWidth: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    description: {
+        fontSize: 16,
+    },
+    footerL: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
+    footerR: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10,
+    },
 });
